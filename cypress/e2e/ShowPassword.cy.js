70fb53f3213f9a4570de8479cbac3a27e5081803
Manolache Alexandru <99614597+undo444@users.noreply.github.com>
2022-07-19 23:13:10 +0300

    Show Password icon functionality

---
+++ cypress/e2e/ShowPassword.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Show password icon functionality', () => {
+  beforeEach(() => {
+    
+    cy.visit('https://victorious-wave-093b83610.1.azurestaticapps.net/#/register')
+  })
+
+  
+  it('Show password icon',()=>{
+
+     cy.get('svg[id=Capa_1]')
+     .eq(0)
+     .click()
+
+     cy.get('svg[id=Capa_1]')
+     .eq(1)
+     .click()
+
+     cy.get('input[id=password]')
+     .type('Test1234$')
+     .should('have.class','sc-crzoAE ezHBjF')
+     
+     cy.get('input[id=confirm-password]')
+     .type('Test1234$')
+     .should('have.class','sc-crzoAE ezHBjF')
+  }
+  
+  )
+
+
+
+})
