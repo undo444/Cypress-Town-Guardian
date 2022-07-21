0ffe2e73082f7485fa887e87ccf7de980cfd4457
Manolache Alexandru <99614597+undo444@users.noreply.github.com>
2022-07-21 16:38:10 +0300

    Verify Profile Page

--- cypress/e2e/Task2/VerifyProfilePage.cy.js
+++ cypress/e2e/Task2/VerifyProfilePage.cy.js
@@ -19,6 +19,33 @@ describe('Verify Login functionality for all users type', () => {
          .contains('Login')
          .should('be.visible')
          .click() 
+          
+        cy.wait(5000) 
+        cy.visit('https://victorious-wave-093b83610.1.azurestaticapps.net/#/profile')
+
+        cy.get('input[id=firstName]')
+          .should('have.value','Alexandru')
+
+        cy.get('input[id=lastName]')
+          .should('have.value','Manolache') 
+
+        cy.get('input[id=address')  
+          .should('have.value','test adress')
+
+        cy.get('input[id=email]')
+          .should('have.value','manolache.alexandru94@gmail.com')  
+
+        cy.get('input[id=personalNumber]')
+          .should('have.value','1940313337435')  
+
+        cy.get('button')  
+          .contains('Save')
+          .should('be.visible')
+
+        
+
+
+
 
          
          
