0d3be6eec99c7ef6e5123b8b180249d21805bbcf
Manolache Alexandru <99614597+undo444@users.noreply.github.com>
2022-07-22 13:19:06 +0300

    Register POM

--- cypress/e2e/RegisterPOM.spec.cy.js
+++ cypress/e2e/RegisterPOM.spec.cy.js
@@ -1,18 +1,28 @@
 import Register from "./PageObject/register";
+const registerpage = new Register();
+let example;
 
 describe('Cypress POM Test suite',function()  {
   
+    before(() => {
+    
+        cy.fixture('example.json').then((data) => {
+           
+            example=data;
+          })
+      })
     it('Register with valid data for "Admin" user and access',function(){ 
         
-     const registerpage = new Register(); 
+     
      registerpage.navigate();
-     registerpage.firstname('Al');
-     registerpage.lastname('Popa');
-     registerpage.email("cici@yahoo.com");
-     registerpage.password('1234');
-     registerpage.confirmpassword('1234')
-     registerpage.personalnumber('6001313019334')
-     registerpage.registerclick();
+     registerpage.firstname.type(example.FirstName);
+     registerpage.lastname.type(example.LastName);
+     registerpage.email.type(example.email);
+     registerpage.password.type(example.password);
+     registerpage.confirmPassword.type(example.ConfirmPassworD);
+     registerpage.personalNumber.type(example.PersonalNumber);
+     registerpage.submit.click();
+
      cy.url().should('be.equal','https://victorious-wave-093b83610.1.azurestaticapps.net/#/register')
     })
     
